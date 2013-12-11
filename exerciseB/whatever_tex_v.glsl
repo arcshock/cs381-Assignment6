9a867b6acec285fe2123655aad5ca6a7d368193b
Paul <beriukay+github@gmail.com>
2013-12-11 13:21:06 -0900

    got rid of texture[0] stuff

--- exerciseB/whatever_tex_v.glsl
+++ exerciseB/whatever_tex_v.glsl
@@ -1,10 +1,10 @@
 // whatever_tex_v.glsl
-// Glenn G. Chappell
-// 1 Nov 2012
+// Bucky Frost and Paul Gentemann
+// Dec 4, 2013
 //
 // For CS 381 Fall 2013
 // GLSL Vertex Shader for use with usetextures.cpp
-// Whatever I Feel Like Doing with Textures
+// glassy
 
 varying vec3 surfpt;       // Point on surface (camera coords)
 varying vec3 surfnorm_un;  // Surface normal (camera coords)
@@ -15,7 +15,7 @@ void main()
     gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
 
     // Find object position (camera coords)
-    vec4 surfpt4 = gl_TextureMatrix[0] * gl_MultiTexCoord0;
+    vec4 surfpt4 = gl_ModelViewMatrix * gl_Vertex;
     surfpt = surfpt4.xyz / surfpt4.w;
 
     // Transform normal vector and ensure it has length 1
